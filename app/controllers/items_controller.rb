d950cc74bc6c6d4891070bc627fbcbc8797fea9f
donrestarone <shashikejayatunge@gmail.com>
2018-09-22 16:48:20 -0400

    can create items and list them

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,39 @@
+class ItemsController < ApplicationController
+	def index
+		user_id = params[:user_id].to_i
+		user = User.find(user_id)
+		user_items = user.items 
+		if user && user.items.count > 0
+			@items = user_items
+		else 
+			render 'errors/you_have_no_items'
+		end
+	end
+	def new	
+		@item = Item.new
+	end
+
+	def create
+		user_id = params[:user_id].to_i
+		user = User.find(user_id)
+		item_name = params[:item][:name]
+		item_condition = params[:item][:condition]
+		price = params[:item][:price].to_f
+		description = params[:item][:description]
+
+		item = user.items.new(name: item_name, condition: item_condition, description: description, price: price)
+		if item.save
+			redirect_to users_items_path
+		else 
+			render 'errors/cannot_create_item'
+		end
+	end
+
+
+	def show
+		user_id = params[:user_id].to_i
+		user = User.find(user_id)
+		item_id = params[:item_id].to_i
+		@item = user.items.find(item_id)
+	end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,20 @@ class UsersController < ApplicationController
 			render 'errors/cannot_create_user'
 		end
 	end
+
+	def edit
+		user_id = params[:user_id].to_i
+		@user = User.find(user_id)
+	end
+
+	def update
+		user_id = params[:user_id].to_i
+		user = User.find(user_id)
+
+		email = params[:user][:email]
+		location = params[:user][:location]
+		geo_location = Place.process_location(location)
+
+		if user.update(email: email, latitude: geo_location[:lat], longitude: geo_location[:lng]) then redirect_to root_path else render 'errors/could_not_update_profile' end
+	end
 end
