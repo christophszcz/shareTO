703c15738c366086fac4106376d579b5accc3ee7
David Ampofo <davidamp@my.yorku.ca>
2018-09-23 01:19:24 -0400

    Created new item page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # for forums
   root :to => 'home#index'
-  
+
   get '/users/new' => 'users#new'
   post 'users/new/create' => 'users#create'
 
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   get '/users/:user_id/edit' => 'users#edit', as: 'user_edit'
   patch '/users/:user_id/update' => 'users#update', as: 'user_update'
-  
+
   get 'users/:user_id/items' => 'items#index', as: 'users_items'
   get 'users/:user_id/items/show/:item_id/' => 'items#show'
   get 'users/:user_id/items/new' => 'items#new', as: 'users_enter_item'
