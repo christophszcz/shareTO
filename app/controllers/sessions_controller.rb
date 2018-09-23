263354a149acf9b289f10612f27d50eb2dc36599
christophszcz <christoph.szczechowicz@gmail.com>
2018-09-23 06:59:54 -0400

    Fix the logout bug.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,9 @@ class SessionsController < ApplicationController
 
 	def destroy
 		user = User.find(session[:user_id])
-		if session[:user_id] then session[:user_id] = nil end 
+		if session[:user_id] 
+			then session[:user_id] = nil
+		end 
+		redirect_to root_path
 	end	
 end
